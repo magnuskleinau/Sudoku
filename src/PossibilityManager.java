5e57371fcc21493ed3d5a7632945a73fd3353f52
Justus <justus.kleinau@ohmoor.de>
2017-02-21 19:10:40 +0100

    Possibility Manager Input

---
+++ src/PossibilityManager.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Justus on 21.02.2017.
+ */
+public class PossibilityManager {
+    public void solve() {
+        int xPosition = 0, yPosition = 0;
+        for (int i = 0; i < 5; i++){
+            while (yPosition < 9){
+                while (xPosition < 9){
+
+                }
+            }
+        }
+    }
+}
